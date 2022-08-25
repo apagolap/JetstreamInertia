63072f31570b43797076392e57ae901229df7fcd
Angel Pagola <57104299+angelpagola@users.noreply.github.com>
2022-08-25 14:34:35 -0500

    :sparkles: Add inertia progress and spinner progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,4 +20,19 @@ createInertiaApp({
     },
 });
 
-InertiaProgress.init({ color: '#4B5563' });
+// InertiaProgress.init({ color: '#4B5563' });
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+
+    // The color of the progress bar.
+    color: '#4B5563',
+
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
+})
